e44ebb2de0b74bfdc661bb8040c1f95e40309889
William O'Beirne <wbobeirne@gmail.com>
2012-02-24 16:32:01 -0500

    Fixed some browser compatibility with IE, changed the .click event handler to .mousedown (Why you do dees IE?)

--- scripts/horiz-accord.js
+++ scripts/horiz-accord.js
@@ -2,17 +2,17 @@ $(document).ready(function(){
     var lastBlock = $("#a1");
     var maxWidth = 750;
     var minWidth = 50;	
-	var dur = 300;
+    var dur = 300;
 	
-	var numEntries = $("#accordion-container").find(".accordion-entry").length;
-	$("#accordion-container").css('width', ((numEntries-1) * minWidth + maxWidth) + 'px');
-	console.log(((numEntries-1) * minWidth) + maxWidth);
+    var numEntries = $("#accordion-container").find(".accordion-entry").length;
+    $("#accordion-container").css('width', ((numEntries-1) * minWidth + maxWidth) + 'px');
 	
-    $(".accordion-entry").click(
+	//IE doesn't support $.click?
+    $(".accordion-entry").mousedown(
       function(){
         $(lastBlock).animate({width: minWidth+"px"}, { queue:false, duration:dur });
-		$(this).animate({width: maxWidth+"px"}, { queue:false, duration:dur });
-		lastBlock = this;
+        $(this).animate({width: maxWidth+"px"}, { queue:false, duration:dur });
+        lastBlock = this;
       }
     );
 });
